e9fe458c991a43a5cc4e9a67fc54a651977e8f59
Robert Zimmerli <robertzimmi@gmail.com>
2025-07-15 16:08:07 -0300

    13

--- routes/home_routes.py
+++ routes/home_routes.py
@@ -8,7 +8,7 @@ home_bp = Blueprint('home_bp', __name__)
 
 @home_bp.route('/')
 def index():
-    return render_template('home.html')
+    return render_template('inicial.html')
 
 from datetime import datetime
 
